cdbaeb9e03a7b3cd62cc0066c87810e235a34363
davidcondori <deycon.22.adm@gmail.com>
2022-10-24 22:21:17 -0400

    primera vez

---
+++ script.js
@@ -0,0 +1,5 @@
+function eurosDolares(valNum) {
+    document.getElementById("inputDolares").value=valNum*1.156236;
+  }
+  function dolaresEuros(valNum) {
+    document.getElementById("inputEuros").value=valNum*0.864875337;
